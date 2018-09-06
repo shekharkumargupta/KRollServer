85b04f517965114a355ae297b3a3c268e1d20b39
Shekhar Kumar <shekharkumargupta@gmail.com>
2018-09-06 21:50:28 +0530

    Commented out some dependencies that we will add gradually

--- src/main/java/com/kroll/KrollServerApplication.java
+++ src/main/java/com/kroll/KrollServerApplication.java
@@ -1,12 +1,19 @@
 package com.kroll;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KrollServerApplication {
+public class KrollServerApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(KrollServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("KrollServerApplication Initiated!");
+
 	}
 }
